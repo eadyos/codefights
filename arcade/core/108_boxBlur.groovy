be8db16778d220202aa0c4b692f3a79a79f94547
Steve Eady <steve@eady.org>
2021-08-03 13:47:16 -0400

    Update 108_boxBlur.groovy

--- arcade/core/108_boxBlur.groovy
+++ arcade/core/108_boxBlur.groovy
@@ -2,9 +2,12 @@
 https://codefights.com/arcade/code-arcade/waterfall-of-integration/5xPitc3yT3dqS7XkP
 
 
-Last night you partied a little too hard. Now there's a black and white photo of you that's about to go viral! You can't let this ruin your reputation, so you want to apply the box blur algorithm to the photo to hide its content.
+Last night you partied a little too hard. Now there's a black and white photo of you that's about to go viral! You can't let this ruin your reputation, 
+so you want to apply the box blur algorithm to the photo to hide its content.
 
-The pixels in the input image are represented as integers. The algorithm distorts the input image in the following way: Every pixel x in the output image has a value equal to the average value of the pixel values from the 3 × 3 square that has its center at x, including x itself. All the pixels on the border of x are then removed.
+The pixels in the input image are represented as integers. The algorithm distorts the input image in the following way: Every pixel x in the output 
+image has a value equal to the average value of the pixel values from the 3 × 3 square that has its center at x, including x itself. 
+All the pixels on the border of x are then removed.
 
 Return the blurred image as an integer, with the fractions rounded down.
 
